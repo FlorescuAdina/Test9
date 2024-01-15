7ddfb1481d6f57c9859389c893b439826f0767da
FlorescuAdina <131195410+FlorescuAdina@users.noreply.github.com>
2024-01-15 20:37:13 +0200

    Update test9cy.js

--- test9cy.js
+++ test9cy.js
@@ -3,7 +3,7 @@ describe('Checking a class on a selected element', () => {
 
 it('Check a value from the input', () => {
 
-cy.visit('https://google.com');
+cy.visit('https://libris.ro');
 
 cy.get('#autoCompleteButton').should('have.class', 'onSearchClick');
 
